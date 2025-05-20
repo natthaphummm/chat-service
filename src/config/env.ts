1e8a3695f02c65aa4f69ede200f3be39537a7f73
Natthaphum Phusong <info.hzelnat@gmail.com>
2025-05-21 02:27:03 +0700

    upate compose

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,11 +2,11 @@ import "dotenv/config";
 
 const mode = process.env.NODE_ENV || "development";
 const port = process.env.PORT || 3000;
-const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/chat-app";
+const dbUrl = process.env.MONGODB_URI || "mongodb://mongodb:27017/chatapp";
 const jwtSecret = process.env.JWT_SECRET || "secret";
 const jwtExpiresIn = process.env.JWT_EXPIRES_IN || "7d";
 
-const redisHost = process.env.REDIS_HOST || "localhost";
+const redisHost = process.env.REDIS_HOST;
 const redisPort = process.env.REDIS_PORT || 6379;
 
 export const config = {
